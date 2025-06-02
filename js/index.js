7ebfd5ef87b6d8db871f81ccf794bee2104749ad
rizkiabdu <117696675+rizkiabdu@users.noreply.github.com>
2025-06-02 09:55:11 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `My Love Intan Hoerinnisa`.split('')
+const text = `My Love Intan Hoerunnisa`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
